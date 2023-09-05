8fb2c01f4cf03d30bd3113fb22ad541ef7398cf2
DanielPark0606 <52310344+DanielPark0606@users.noreply.github.com>
2023-09-05 13:20:00 -0500

    Change 2

--- src/assignment1/Problem1.java
+++ src/assignment1/Problem1.java
@@ -3,22 +3,37 @@ package assignment1;
 // find the greates product using at most n digits
 import java.util.Scanner;
 public class Problem1 {
-    public int max_product(int n, String s){
-        int max_product = 0;
+    public int max_product(int n, String s) {
+        int max_product = -1;
+        int curr_product = 1;
 
-        for(int i = 0; i < s.length(); i++){
-
-            int curr_product = 1;
-            for(int j = i; j < i + n && j < s.length(); j++) {
-                curr_product *= Character.getNumericValue(s.charAt(j));
+        for(int i = 0; i < n; i++){
+            int digit = Character.getNumericValue(s.charAt(i));
+            if(digit != 0) {
+                curr_product *= digit;
             }
-            if(curr_product > max_product){
-                max_product = curr_product;
+        }
+        max_product = Math.max(max_product, curr_product);
+
+        for (int i = n; i < s.length(); i++) {
+            int digitToRemove = Character.getNumericValue(s.charAt(i - n));
+            int digitToAdd = Character.getNumericValue(s.charAt(i));
+
+            if (digitToRemove != 0) {
+                curr_product /= digitToRemove;
+            }
+
+            if (digitToAdd != 0) {
+                curr_product *= digitToAdd;
+            }
+            max_product = Math.max(max_product, curr_product);
+            // check if digits in windows are zeroes
+            if(curr_product == 0){
+                return 0;
             }
         }
         return max_product;
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -28,8 +43,6 @@ public class Problem1 {
         String s = scanner.nextLine();
 
         Problem1 p1 = new Problem1();
-        // constraint s.length = 1000 and 2 <= n <= 20
-        // s.length() == 1000 &&
         if (2 <= n && n <= 20) {
             int result = p1.max_product(n, s);
             System.out.println(result);
